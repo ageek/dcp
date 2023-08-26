c55b78f1c79917bd5420b683de22eae1e26dcf79
ageek <re.90@live.com>
2023-08-26 08:21:53 +0530

    added DP approach with O(n) run time

--- 007.py
+++ 007.py
@@ -22,6 +22,35 @@ def countEncodings(msg):
    
     return total
 
-msg='1234'
+# using DP (with tabulation)
+# complexity = O(n)
+def countEncodingsDP(s):
+    n = len(s)
+    if n == 0 or s[0] == '0':
+        return 0
+
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        if s[i - 1] != '0':
+            dp[i] += dp[i - 1]
+
+        two_digit = int(s[i - 2:i])
+        if 10 <= two_digit <= 26:
+            dp[i] += dp[i - 2]
+
+    return dp[n]
+
+
+
+    
+    
+
+msg='123412'
 count=countEncodings(msg)
 print(f'Total encodings found = {count:}')
+
+countDP=countEncodingsDP(msg)
+print(f'Total encodings found = {countDP:}')
